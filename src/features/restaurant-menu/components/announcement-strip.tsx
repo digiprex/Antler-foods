7f54fc2e1b36ffa20999d07e75b7b0bd1111562c
Natasha Vyas <natasha.vyas@digi-prex.com>
2026-03-26 11:19:16 +0530

    fixed build

--- src/features/restaurant-menu/components/announcement-strip.tsx
+++ src/features/restaurant-menu/components/announcement-strip.tsx
@@ -4,7 +4,7 @@ interface AnnouncementStripProps {
 
 export function AnnouncementStrip({ text }: AnnouncementStripProps) {
   return (
-    <div className="bg-black px-4 py-3 text-center text-sm font-semibold text-white">
+    <div className="bg-black px-4 pb-3 pt-6 text-center text-sm font-semibold text-white">
       {text}
     </div>
   );
